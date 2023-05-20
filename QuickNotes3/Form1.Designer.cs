2bf16305ff5d5bba69b1c83d42e47ac516cb81a4
Sewon Kim <sewonkim0black@gmail.com>
2023-05-19 23:04:55 -0400

    basic UI complete, color autocoding complete save/load incomplete

--- QuickNotes3/Form1.Designer.cs
+++ QuickNotes3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuickNotes3
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Doc = new System.Windows.Forms.RichTextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.DocPath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Doc
+            // 
+            this.Doc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Doc.BackColor = System.Drawing.Color.Black;
+            this.Doc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Doc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Doc.ForeColor = System.Drawing.Color.White;
+            this.Doc.Location = new System.Drawing.Point(0, 22);
+            this.Doc.Name = "Doc";
+            this.Doc.Size = new System.Drawing.Size(385, 238);
+            this.Doc.TabIndex = 0;
+            this.Doc.TabStop = false;
+            this.Doc.Text = "";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.BackColor = System.Drawing.Color.Black;
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.ForeColor = System.Drawing.Color.White;
+            this.SaveButton.Location = new System.Drawing.Point(307, 0);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(77, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.TabStop = false;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadButton.BackColor = System.Drawing.Color.Black;
+            this.LoadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadButton.ForeColor = System.Drawing.Color.White;
+            this.LoadButton.Location = new System.Drawing.Point(229, 0);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(80, 23);
+            this.LoadButton.TabIndex = 2;
+            this.LoadButton.TabStop = false;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = false;
+            // 
+            // DocPath
+            // 
+            this.DocPath.AutoSize = true;
+            this.DocPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DocPath.ForeColor = System.Drawing.Color.White;
+            this.DocPath.Location = new System.Drawing.Point(0, 3);
+            this.DocPath.Name = "DocPath";
+            this.DocPath.Size = new System.Drawing.Size(38, 16);
+            this.DocPath.TabIndex = 3;
+            this.DocPath.Text = "Path";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(385, 260);
+            this.Controls.Add(this.DocPath);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.Doc);
+            this.Name = "Form";
+            this.Text = "QuickNotes3";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox Doc;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Label DocPath;
     }
 }
 
--- QuickNotes3/Form1.cs
+++ QuickNotes3/Form1.cs
@@ -7,14 +7,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace QuickNotes3
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        private Color color1 = Color.White;
+        private Color color2 = Color.FromArgb(180, 180, 180);
+        private Color color3 = Color.FromArgb(120, 120, 120);
+
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            //closing: save to data
+            this.FormClosing += SetData;
+            //doc position change: set new color
+            Doc.SelectionChanged += SetColor;
+
+            //get from data: read file
+            string[] data = File.ReadAllText("data.txt").Split(' ');
+            //set position by data
+            this.Location = new Point(
+                int.Parse(data[0]), 
+                int.Parse(data[1])
+            );
+            //set size by data
+            this.Size = new Size(
+                int.Parse(data[2]),
+                int.Parse(data[3])
+            );
+        }
+
+        private void SetData(object sender, EventArgs e)
+        {
+            //get position
+            int posX = this.Location.X;
+            int posY = this.Location.Y;
+            //get size
+            int sizeX = this.Size.Width;
+            int sizeY = this.Size.Height;
+
+            //save to file
+            File.WriteAllText("data.txt", 
+                posX + " " + posY + " " + sizeX + " " + sizeY    
+            );
+        }
+
+        private void Reload()
+        {
+            //get selection index
+            int selectionIndex = Doc.SelectionStart;
+
+            //get all lines
+            string[] lines = Doc.Text.Split('\n');
+            //clear doc
+            Doc.Text = "";
+
+            //write lines: colors
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                //2 tabs: color3
+                if (line.Length >= 2 && line[0] == '\t' && line[1] == '\t')
+                {
+                    Doc.SelectionColor = color3;
+                }
+                //1 tab: color2
+                else if (line.Length >= 1 && line[0] == '\t')
+                {
+                    Doc.SelectionColor = color2;
+                }
+                //default: color1
+                else
+                {
+                    Doc.SelectionColor = color1;
+                }
+
+                //add to doc
+                if (i == lines.Length - 1)
+                {
+                    Doc.AppendText(line);
+                }
+                else
+                {
+                    Doc.AppendText(line + "\n");
+                }
+            }
+
+            //scroll to caret by selectionIndex
+            Doc.SelectionStart = selectionIndex;
+            Doc.ScrollToCaret();
+        }
+
+        private void SetColor(object sender, EventArgs e)
+        {
+            //get lines
+            string[] lines = Doc.Text.Split('\n');
+            //get pos
+            int pos = Doc.SelectionStart;
+            //get line index: line iteration
+            int lineIndex = -1;
+            int currPos = 0;
+            while (currPos <= pos)
+            {
+                //increment lineIndex
+                lineIndex++;
+                //increment currPos by len + 1
+                currPos += lines[lineIndex].Length + 1;
+            }
+
+            string line = lines[lineIndex];
+            //2 tabs: color3
+            if (line.Length >= 2 && line[0] == '\t' && line[1] == '\t')
+            {
+                Doc.SelectionColor = color3;
+            }
+            //1 tab: color2
+            else if (line.Length >= 1 && line[0] == '\t')
+            {
+                Doc.SelectionColor = color2;
+            }
+            //default: color1
+            else
+            {
+                Doc.SelectionColor = color1;
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            Reload();
         }
     }
 }
