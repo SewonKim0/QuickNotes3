be47e292c35a8fc6c09b2a3fa0ce545ede051e84
Sewon Kim <sewonkim0black@gmail.com>
2023-07-10 21:59:23 -0400

    Find case insensitive, arrow controls added

--- QuickNotes3/Form1.cs
+++ QuickNotes3/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace QuickNotes3
 {
@@ -17,10 +18,17 @@ namespace QuickNotes3
     {
         // Doc text colors
         private Color color1 = Color.White;
-        private Color color2 = Color.FromArgb(180, 180, 180);
-        private Color color3 = Color.FromArgb(120, 120, 120);
-        //Current doc path
+        private Color color2 = Color.FromArgb(190, 190, 190);
+        private Color color3 = Color.FromArgb(140, 140, 140);
+        // Current doc path
         private string docPath = "";
+
+        // Find: Start Indices
+        private List<int> findIndices = new List<int>();
+        // Find: Curr Index
+        private int findIndex = 0;
+        // Find: Curr Keyword
+        private string findKeyword = "";
 
         // Doc scroll down control
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
@@ -36,23 +44,34 @@ namespace QuickNotes3
 
         private void Form_Load(object sender, EventArgs e)
         {
-            //closing: save to data
-            this.FormClosing += SetData;
-            //closing: save document
-            this.FormClosing += SaveDoc;
-            //doc position change: set new color
-            Doc.SelectionChanged += SetColor;
-            //doc link click: go to link
-            Doc.LinkClicked += Doc_LinkClicked;
+            // hide find-related controls
+            FindInput.Visible = false;
+            FindUpButton.Visible = false;
+            FindDownButton.Visible = false;
 
-            //get from data: read file
+            // closing: save to data
+            this.FormClosing += SetData;
+            // closing: save document
+            this.FormClosing += SaveDoc;
+
+            // doc position change: set new color
+            Doc.SelectionChanged += SetColor;
+            // doc link click: go to link
+            Doc.LinkClicked += Doc_LinkClicked;
+            // doc shortcut keys & find shortcuts
+            Doc.KeyDown += Doc_KeyDown;
+
+            // find input enter: set find data
+            FindInput.KeyPress += FindInput_KeyPress;
+
+            // get from data: read file
             string[] data = File.ReadAllText("data.txt").Split(' ');
-            //set position by data
+            // set position by data
             this.Location = new Point(
                 int.Parse(data[0]), 
                 int.Parse(data[1])
             );
-            //set size by data
+            // set size by data
             this.Size = new Size(
                 int.Parse(data[2]),
                 int.Parse(data[3])
@@ -96,36 +115,36 @@ namespace QuickNotes3
 
         private void Reload()
         {
-            //get selection index
+            // get selection index
             int selectionIndex = Doc.SelectionStart;
 
-            //get all lines
+            // get all lines
             string[] lines = Doc.Text.Split('\n');
-            //clear doc
+            // clear doc
             Doc.Text = "";
 
-            //write lines: colors
+            // write lines: colors
             for (int i = 0; i < lines.Length; i++)
             {
                 string line = lines[i];
 
-                //2 tabs: color3
+                // 2 tabs: color3
                 if (line.Length >= 2 && line[0] == '\t' && line[1] == '\t')
                 {
                     Doc.SelectionColor = color3;
                 }
-                //1 tab: color2
+                // 1 tab: color2
                 else if (line.Length >= 1 && line[0] == '\t')
                 {
                     Doc.SelectionColor = color2;
                 }
-                //default: color1
+                // default: color1
                 else
                 {
                     Doc.SelectionColor = color1;
                 }
 
-                //add to doc
+                // add to doc
                 if (i == lines.Length - 1)
                 {
                     Doc.AppendText(line);
@@ -155,7 +174,7 @@ namespace QuickNotes3
                     SendMessage(Doc.Handle, EM_SCROLL, (IntPtr)SB_LINEUP, IntPtr.Zero);
                 }
             }
-            //if bottom: scroll up by (remaining - (visible / 2))
+            // if bottom: scroll up by (remaining - (visible / 2))
             else
             {
                 int remainingLines = numTotalLines - currLine;
@@ -202,6 +221,7 @@ namespace QuickNotes3
             }
         }
 
+        /* Save Button: Full saving procedure */
         private void SaveButton_Click(object sender, EventArgs e)
         {
             string path = docPath;
@@ -292,14 +312,14 @@ namespace QuickNotes3
 
         private void DeleteButton_Click(object sender, EventArgs e)
         {
-            //get file path to delete
+            // get file path to delete
             OpenFileDialog deleteFileDialog = new OpenFileDialog();
             deleteFileDialog.Filter = "Text Files (*.txt)|*.txt";
             deleteFileDialog.Title = "Delete File";
 
             string deletePath = "";
 
-            //if result is ok, set file path
+            // if result is ok, set file path
             if (deleteFileDialog.ShowDialog() == DialogResult.OK)
             {
                 deletePath = deleteFileDialog.FileName;
@@ -309,7 +329,7 @@ namespace QuickNotes3
                 return;
             }
 
-            //if file path == current path: clear (doc, docPath, DocPath)
+            // if file path == current path: clear (doc, docPath, DocPath)
             if (deletePath.Equals(docPath))
             {
                 Doc.Text = "";
@@ -317,7 +337,7 @@ namespace QuickNotes3
                 DocPath.Text = "";
             }
 
-            //delete
+            // delete
             File.Delete(deletePath);
         }
 
@@ -326,9 +346,177 @@ namespace QuickNotes3
             //
         }
 
+        /* When link clicked in doc */
         private void Doc_LinkClicked(object sender, LinkClickedEventArgs e)
         {
             Process.Start(e.LinkText);
+        }
+
+        /* Toggle visibility of find interface */
+        private void FindButton_Click(object sender, EventArgs e)
+        {
+            // if not visible: show
+            if (FindInput.Visible == false)
+            {
+                FindInput.Visible = true;
+                FindUpButton.Visible = true;
+                FindDownButton.Visible = true;
+            }
+            // if visible: hide and reset find data
+            else
+            {
+                FindInput.Visible = false;
+                FindUpButton.Visible = false;
+                FindDownButton.Visible = false;
+                findKeyword = "";
+                findIndices.Clear();
+                findIndex = 0;
+            }
+        }
+
+        /* Find keyword entered: create list of results */
+        private void FindInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // ensure enter was pressed
+            if (e.KeyChar != (char) Keys.Return)
+            {
+                return;
+            }
+            // edge case: input is blank
+            if (FindInput.Text.Equals(""))
+            {
+                return;
+            }
+
+            // reset findIndex & findIndices
+            findIndex = 0;
+            findIndices.Clear();
+
+            // find all start indices of keyword
+            findKeyword = FindInput.Text;
+            String text = Doc.Text;
+            int currIndex = 0;
+            while (text.IndexOf(findKeyword, currIndex, StringComparison.OrdinalIgnoreCase) != -1)
+            {
+                findIndices.Add(text.IndexOf(findKeyword, currIndex, StringComparison.OrdinalIgnoreCase));
+                currIndex = text.IndexOf(findKeyword, currIndex, StringComparison.OrdinalIgnoreCase);
+                currIndex += findKeyword.Length;
+            }
+            
+            // if no indices: stop
+            if (findIndices.Count == 0)
+            {
+                return;
+            }
+            // select first index
+            else
+            {
+                Doc.Select();
+                Doc.SelectionStart = findIndices[findIndex];
+                Doc.SelectionLength = findKeyword.Length;
+            }
+        }
+
+        /* Go up results of Find */
+        private void FindUpButton_Click(object sender, EventArgs e)
+        {
+            // if find list empty: stop
+            if (findIndices.Count == 0)
+            {
+                return;
+            }
+
+            // decrement index
+            if (findIndex == 0)
+            {
+                findIndex = findIndices.Count - 1;
+            }
+            else
+            {
+                findIndex--;
+            }
+
+            // select and go to position
+            Doc.Select();
+            Doc.SelectionStart = findIndices[findIndex];
+            Doc.SelectionLength = findKeyword.Length;
+        }
+
+        /* Go down results of Find */
+        private void FindDownButton_Click(object sender, EventArgs e)
+        {
+            // if find list empty: stop
+            if (findIndices.Count == 0)
+            {
+                return;
+            }
+
+            // increment index
+            findIndex = (findIndex + 1) % findIndices.Count;
+
+            // select and go to position
+            Doc.Select();
+            Doc.SelectionStart = findIndices[findIndex];
+            Doc.SelectionLength = findKeyword.Length;
+        }
+
+        /* Handling for Doc keyboard & find shortcuts */
+        private void Doc_KeyDown(object sender, KeyEventArgs e)
+        {
+            // ctrl + s: save doc
+            if (e.Control && char.ToLower((char)e.KeyCode) == 's')
+            {
+                SaveButton_Click(null, null);
+            }
+
+            // ctrl + r: reload doc
+            if (e.Control && char.ToLower((char)e.KeyCode) == 'r')
+            {
+                Reload();
+                e.Handled = true;
+            }
+
+            // ctrl + f: find in doc
+            if (e.Control && char.ToLower((char)e.KeyCode) == 'f')
+            {
+                // make find interface visible
+                FindInput.Visible = true;
+                FindDownButton.Visible = true;
+                FindUpButton.Visible = true;
+
+                // focus to FindInput
+                FindInput.Select();
+            }
+
+            // up arrow: move up in find
+            if (e.KeyCode == Keys.Up)
+            {
+                // validate find mode
+                string selected = Doc.Text.Substring(Doc.SelectionStart, Doc.SelectionLength);
+                if (!(selected.Equals(findKeyword, StringComparison.OrdinalIgnoreCase)) || findKeyword.Equals(""))
+                {
+                    return;
+                }
+
+                // go up
+                FindUpButton_Click(null, null);
+                e.Handled = true;
+            }
+
+            // down arrow: move down in find
+            if (e.KeyCode == Keys.Down)
+            {
+                // validate find mode
+                string selected = Doc.Text.Substring(Doc.SelectionStart, Doc.SelectionLength);
+                if (!(selected.Equals(findKeyword, StringComparison.OrdinalIgnoreCase)) || findKeyword.Equals(""))
+                {
+                    return;
+                }
+
+                // go down
+                FindDownButton_Click(null, null);
+                e.Handled = true;
+            }
         }
     }
 }
