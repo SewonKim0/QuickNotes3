49fe69557271218c138480a4bb79a17fab067e26
Sewon Kim <sewonkim0black@gmail.com>
2023-07-29 00:54:15 -0400

    static file build error fixed

--- QuickNotes3/Program.cs
+++ QuickNotes3/Program.cs
@@ -16,7 +16,7 @@ namespace QuickNotes3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            Application.Run(new MainForm());
         }
     }
 }
